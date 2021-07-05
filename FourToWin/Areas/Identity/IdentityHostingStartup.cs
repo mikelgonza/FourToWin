b7a294b72a0e65d914297677ad66e44c54334e11
mikel-dev <ibarrondomikel@gmail.com>
2021-07-05 10:29:05 +0200

    Generate all identity pages

---
+++ FourToWin/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FourToWin.Data;
+using FourToWin.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FourToWin.Areas.Identity.IdentityHostingStartup))]
+namespace FourToWin.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
