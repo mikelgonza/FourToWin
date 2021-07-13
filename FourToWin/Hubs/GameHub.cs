9096168b92bdc523a766b5ac0d74189ca1cc91b8
mikel-dev <ibarrondomikel@gmail.com>
2021-07-13 16:17:49 +0200

    random player and some fixes

--- FourToWin/Hubs/GameHub.cs
+++ FourToWin/Hubs/GameHub.cs
@@ -8,41 +8,80 @@ namespace FourToWin.Hubs
 {
     public class GameHub : Hub
     {
-        private static List<string> lobbies = new List<string>();
-        private static List<string> customLobbies = new List<string>();
+        //private static List<string> lobbies = new List<string>();
+        //private static List<string> customLobbies = new List<string>();
+
+        //private static Dictionary<string, int> lobbies = new Dictionary<string, int>();
+        private static Dictionary<string, int> customLobbies = new Dictionary<string, int>();
 
         private static string player1Id, player1Nickname, player1Image;
         private static string player2Id, player2Nickname, player2Image;
 
         public async void CreateGame(string userId, string userNickname, string userImage)
         {
-            player1Id = userId;
-            player1Nickname = userNickname;
-            player1Image = userImage;
-            string lobbyId = GenerateLobbyId();
-            string player = "p1";
-            customLobbies.Add(lobbyId);
-            await AddToGroup(lobbyId);
-            await Clients.Caller.SendAsync("GetPlayerNum", player);
-            await Clients.Caller.SendAsync("GetLobbyId", lobbyId);
-            await Clients.Caller.SendAsync("GetUser1", player1Id, player1Nickname, player1Image);
-        }
+            int randomPlayer = GenerateRandomPlayer();
 
-        public async void JoinGame(string lobbyId, string userId, string userNickname, string userImage)
-        {
-            if (customLobbies.Contains(lobbyId))
+            if (randomPlayer == 1)
             {
-                player2Id = userId;
-                player2Nickname = userNickname;
-                player2Image = userImage;
-                customLobbies.Remove(lobbyId);
-                string player = "p2";
+                player1Id = userId;
+                player1Nickname = userNickname;
+                player1Image = userImage;
+                string lobbyId = GenerateLobbyId();
+                string player = "p1";
+                customLobbies.Add(lobbyId, 1);
                 await AddToGroup(lobbyId);
                 await Clients.Caller.SendAsync("GetPlayerNum", player);
                 await Clients.Caller.SendAsync("GetLobbyId", lobbyId);
                 await Clients.Caller.SendAsync("GetUser1", player1Id, player1Nickname, player1Image);
-                await Clients.All.SendAsync("GetUser2", player2Id, player2Nickname, player2Image);
-                await Clients.Group(lobbyId).SendAsync("Ready");
+            }
+            else
+            {
+                player2Id = userId;
+                player2Nickname = userNickname;
+                player2Image = userImage;
+                string lobbyId = GenerateLobbyId();
+                string player = "p2";
+                customLobbies.Add(lobbyId, 2);
+                await AddToGroup(lobbyId);
+                await Clients.Caller.SendAsync("GetPlayerNum", player);
+                await Clients.Caller.SendAsync("GetLobbyId", lobbyId);
+                await Clients.Caller.SendAsync("GetUser2", player2Id, player2Nickname, player2Image);
+            }
+        }
+
+        public async void JoinGame(string lobbyId, string userId, string userNickname, string userImage)
+        {
+            if (customLobbies.ContainsKey(lobbyId))
+            {
+                int firstPlayer = customLobbies[lobbyId];
+                if (firstPlayer == 1)
+                {
+                    player2Id = userId;
+                    player2Nickname = userNickname;
+                    player2Image = userImage;
+                    customLobbies.Remove(lobbyId);
+                    string player = "p2";
+                    await AddToGroup(lobbyId);
+                    await Clients.Caller.SendAsync("GetPlayerNum", player);
+                    await Clients.Caller.SendAsync("GetLobbyId", lobbyId);
+                    await Clients.Caller.SendAsync("GetUser1", player1Id, player1Nickname, player1Image);
+                    await Clients.All.SendAsync("GetUser2", player2Id, player2Nickname, player2Image);
+                    await Clients.Group(lobbyId).SendAsync("Ready");
+                }
+                else if (firstPlayer == 2)
+                {
+                    player1Id = userId;
+                    player1Nickname = userNickname;
+                    player1Image = userImage;
+                    customLobbies.Remove(lobbyId);
+                    string player = "p1";
+                    await AddToGroup(lobbyId);
+                    await Clients.Caller.SendAsync("GetPlayerNum", player);
+                    await Clients.Caller.SendAsync("GetLobbyId", lobbyId);
+                    await Clients.All.SendAsync("GetUser1", player1Id, player1Nickname, player1Image);
+                    await Clients.Caller.SendAsync("GetUser2", player2Id, player2Nickname, player2Image);
+                    await Clients.Group(lobbyId).SendAsync("Ready");
+                }
             }
             else
             {
@@ -50,40 +89,40 @@ namespace FourToWin.Hubs
             }
         }
 
-        public async void QuickGame(string userId, string userNickname, string userImage)
-        {
-            string lobbyId;
-            string player;
+        //public async void QuickGame(string userId, string userNickname, string userImage)
+        //{
+        //    string lobbyId;
+        //    string player;
 
-            if (lobbies.Count == 0)
-            {
-                player1Id = userId;
-                player1Nickname = userNickname;
-                player1Image = userImage;
-                player = "p1";
-                lobbyId = GenerateLobbyId();
-                lobbies.Add(lobbyId);
-                await AddToGroup(lobbyId);
-                await Clients.Caller.SendAsync("GetPlayerNum", player);
-                await Clients.Caller.SendAsync("GetLobbyId", lobbyId);
-                await Clients.Caller.SendAsync("GetUser1", player1Id, player1Nickname, player1Image);
-            }
-            else
-            {
-                player2Id = userId;
-                player2Nickname = userNickname;
-                player2Image = userImage;
-                player = "p2";
-                lobbyId = lobbies[0];
-                lobbies.Remove(lobbyId);
-                await AddToGroup(lobbyId);
-                await Clients.Caller.SendAsync("GetPlayerNum", player);
-                await Clients.Caller.SendAsync("GetLobbyId", lobbyId);
-                await Clients.Caller.SendAsync("GetUser1", player1Id, player1Nickname, player1Image);
-                await Clients.All.SendAsync("GetUser2", player2Id, player2Nickname, player2Image);
-                await Clients.Group(lobbyId).SendAsync("Ready");
-            }
-        }
+        //    if (lobbies.Count == 0)
+        //    {
+        //        player1Id = userId;
+        //        player1Nickname = userNickname;
+        //        player1Image = userImage;
+        //        player = "p1";
+        //        lobbyId = GenerateLobbyId();
+        //        lobbies.Add(lobbyId);
+        //        await AddToGroup(lobbyId);
+        //        await Clients.Caller.SendAsync("GetPlayerNum", player);
+        //        await Clients.Caller.SendAsync("GetLobbyId", lobbyId);
+        //        await Clients.Caller.SendAsync("GetUser1", player1Id, player1Nickname, player1Image);
+        //    }
+        //    else
+        //    {
+        //        player2Id = userId;
+        //        player2Nickname = userNickname;
+        //        player2Image = userImage;
+        //        player = "p2";
+        //        lobbyId = lobbies[0];
+        //        lobbies.Remove(lobbyId);
+        //        await AddToGroup(lobbyId);
+        //        await Clients.Caller.SendAsync("GetPlayerNum", player);
+        //        await Clients.Caller.SendAsync("GetLobbyId", lobbyId);
+        //        await Clients.Caller.SendAsync("GetUser1", player1Id, player1Nickname, player1Image);
+        //        await Clients.All.SendAsync("GetUser2", player2Id, player2Nickname, player2Image);
+        //        await Clients.Group(lobbyId).SendAsync("Ready");
+        //    }
+        //}
 
         public async Task SendMessageToGroup(string message, string group, string user)
         {
@@ -109,6 +148,12 @@ namespace FourToWin.Hubs
         {
             int lobbyId = new Random().Next(10000, 99999);
             return lobbyId.ToString();
+        }
+
+        private int GenerateRandomPlayer()
+        {
+            int random = new Random().Next(1, 3);
+            return random;
         }
 
     }
